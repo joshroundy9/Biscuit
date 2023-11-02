689e712cd407f1500ac235228f42456fcf9e5ad0
joshroundy9 <104024433+joshroundy9@users.noreply.github.com>
2023-11-02 16:32:03 -0400

    Finished login page

--- BiscuitApp/initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
+++ BiscuitApp/initial/src/main/java/com/example/securingweb/WebSecurityConfig.java
@@ -18,7 +18,7 @@ public class WebSecurityConfig {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
 			.authorizeHttpRequests((requests) -> requests
-				.requestMatchers("/", "/home", "/css/**","/sign-up").permitAll()
+				.requestMatchers("/", "/home", "/css/**","/signup").permitAll()
 				.anyRequest().authenticated()
 			)
 			.formLogin((form) -> form
